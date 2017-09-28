b10cb8f28a90ced31cf4e5570d72234851d32e9a
shaneisaak <shaneisaak@172.16.41.144>
2017-09-28 10:13:30 -0500

    Uploading to github

---
+++ PalindromeChecker/src/palindromeChecker/PalindromeChecker.java
@@ -0,0 +1,48 @@
+package palindromeChecker;
+
+import java.util.Scanner;
+
+public class PalindromeChecker 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a palindrome:");
+		String nextString = in.next();
+		//if the string is a palindrome, this will display "It is a palindrome"
+		if(checkNext(nextString))
+		{
+			System.out.println("It is a palindrome");
+		}
+		else
+		{
+			System.out.println("It is not a palindrome");
+		}		
+	}
+	//this method checks to see if the first and the last character of the string are the same
+	//if they are, this method calls itself again and keeps checking first and last characters until true or false is returned
+	public static Boolean checkNext(String str)
+	{
+
+		if(str.charAt(0) == str.charAt(str.length() - 1))
+		{
+			if(str.length() == 3 || str.length() == 2)
+			{
+				return true;
+			}
+			else
+			{
+				//this returns the string without the first and last letters on it
+				return checkNext(str.substring(1, str.length() - 1));
+			}
+			
+		}
+		else
+		{
+			return false;
+		}
+		
+	}
+
+}
